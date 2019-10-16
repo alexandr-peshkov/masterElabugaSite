364177a22f66495af4b4bb2e85e5053d889d79c3
Alexander Peshkov <89267779090@mail.ru>
2019-10-16 12:49:10 +0300

    add menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-    $("#phone").mask(" +7 (999)-999-99-99");
-});
+
+//TODO мобильное меню js
+
+window.onload = function () {
+    
+    $(document).ready(function () {
+        $("#phone").mask(" +7 (999)-999-99-99");
+    });
+
+    document.querySelector(".mob-menu").onclick = showMenu;
+    document.querySelector(".mob-menu").onclick = hideMenu;
+
+    function showMenu() {
+        document.querySelector(".nav-list").style.right = "0";
+    };
+    function hideMenu() {
+        document.querySelector(".nav-list").style.right = "-250px";
+    };
+};
